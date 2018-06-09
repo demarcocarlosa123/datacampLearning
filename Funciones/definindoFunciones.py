127ba5ab8d9f74fab88ec2454d101ea400e19135
demarcocarlosa123 <demarcocarlosa@gmail.com>
2018-06-09 18:51:51 -0300

    Modificaciones que hice a medida que estudiaba 3

--- Funciones/definindoFunciones.py
+++ Funciones/definindoFunciones.py
@@ -1,3 +1,14 @@
+# Para devolver multiples valores de las funciones se usa una estructura llamada tupla
+tupla1 = ("unaValor", "OtroValor", "Otro valor mas")
+print(tupla1)
+
+val1, val2, val3 = tupla1
+print(val2)
+print(tupla1[1])
+
+
+
+
 def gritar(palabra1, palabra2):
     """Le agrega a palabra pasada tres exclamaciones"""
     tupla = (palabra1 + "!!!", palabra2 + "!!!")
@@ -7,10 +18,3 @@ print(grito1)
 print(grito2)
 
 
-# Para devolver multiples valores de las funciones se usa una estructura llamada tupla
-tupla1 = ("unaValor", "OtroValor", "Otro valor mas")
-print(tupla1)
-
-val1, val2, val3 = tupla1
-print(val2)
-print(tupla1[1])
--- Funciones/lambdaFunctions.py
+++ Funciones/lambdaFunctions.py
@@ -1,18 +1,39 @@
+#Lamda funcions es una forma simple de escribir una breve funcion en una sola linea.
 gritar = (lambda palabra, veces: palabra * veces)
 print(gritar("Hola ", 5))
 
 
-#Lambda functions sirven para aplicar una simple funcion sobre un array
+#map() sirven para aplicar una funcion lambda sobre un array
+#map() recibe dos parametros, una funcion y una lista. La funcion puede ser tanto un lambda como una funcion comun
 spells = ["protego", "accio", "expecto patronum", "legilimens"]
 spellsRepetidos = map(lambda s: (s + " ") * 3, spells)
 print(type(spellsRepetidos))
 print(spellsRepetidos)
 spellsRepetidosLista = list(spellsRepetidos)
+print(spellsRepetidosLista)
+
+def multiplicarPalabra(cantidadVeces):
+    """A la palabra pasada por parametro se le agrega un espacio y a eso se lo multiplique por 3"""
+    def multiplicar(palabra):
+        return (palabra + " ") * cantidadVeces
+    return multiplicar
+
+porTres = multiplicarPalabra(3)
+spellsRepetidos = map(porTres, spells)
+print(list(spellsRepetidos))
+
+
+
 
 #Otro uso comun es para filtrar arrays
 fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
-cortos = filter(lambda member: len(member)>6 , fellowship)
-print(cortos)
+miembrosLargos = filter(lambda member: len(member)>6 , fellowship)
+print(miembrosLargos)
+print(list(miembrosLargos))
+
+isCorto = (lambda elemento: len(elemento)<=6)
+miembrosCortos = filter(lambda elemento: len(elemento)<=6, fellowship)
+print(list(miembrosCortos))
 
 #La otra utilidad que tiene lambda es aplicarlos sobre reduce.
 # Import reduce from functools
