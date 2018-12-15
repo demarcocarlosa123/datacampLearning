34e4727a66eae6001a76ffc888b5b2f7a35beaec
demarcocarlosa123 <demarcocarlosa@gmail.com>
2018-12-15 20:44:20 -0300

    Refrescando python

--- Basico/comprenhension and Generators/Comprenhension_Dic.py
+++ Basico/comprenhension and Generators/Comprenhension_Dic.py
@@ -1,7 +1,7 @@
 pos = {num:-num for num in range(10)}
 print(pos)
 
-
+### A partir de una lista creo un diccionario.
 fellowship = ['frodo', 'samwise', 'merry', 'aragorn', 'legolas', 'boromir', 'gimli']
 dicFellowship = {elemento:len(elemento) for elemento in fellowship}
 print(dicFellowship)
