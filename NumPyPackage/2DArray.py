a2c2fbe8454f1e8d7abdb8e68eb19aead44f3c98
demarcocarlosa123 <demarcocarlosa@gmail.com>
2018-12-16 20:10:23 -0300

    Refrescando python

--- NumPyPackage/2DArray.py
+++ NumPyPackage/2DArray.py
@@ -15,6 +15,7 @@ print(basket_np.shape)
 np_mat = np.array([[1, 2],
                    [3, 4],
                    [5, 6]])
+print(np_mat.shape)
 print(np_mat * 2)
 print(np_mat + np.array([10, 5]))
 print(np_mat + np_mat)
