1b3323d5b6bbc90f40f1ff420967ea0679bfeeff
demarcocarlosa123 <demarcocarlosa@gmail.com>
2019-04-12 05:59:53 -0300

    CleaningData1

---
+++ CleaningData/convertingDataTypes.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+tips = pd.read_csv('files/tips.csv')
+print(tips.info())
+
+### Hay columnas que pueden ser convertidas a CATEGORIAS.
+### Hacerlo ahorra memoria.
+### Se debe aplicar sobre un str la funcion astype('category')
+
+tips.sex = tips.sex.astype('category')
+tips.smoker = tips.smoker.astype('category')
+print(tips.info())
+
+
+
+print('---------------------1')
+### Cuando vemos que hay campos que deben figurar como numericos (int o float,
+### pero aparecen como object (string en python), esto indica que hay data corrupta.
+### entonces o exploramos la data a ver que onda, o forzamos a que sea numerico.
+
+bad = pd.read_csv('files/tipsBad.csv', sep=';')
+print(bad.info())
+print(bad.head(10))
+
+bad['total_bill'] = pd.to_numeric(bad['total_bill'], errors='coerce')
+bad.tip = pd.to_numeric(bad.tip, errors='coerce')
+print(bad.info())
+print(bad.head(10))
